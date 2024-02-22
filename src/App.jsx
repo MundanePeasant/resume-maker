5d6745ed3c7aebd109c80db2eec645e3e668cb30
Mundane <mundanepeasant@gmail.com>
2024-02-21 20:34:00 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './styles/App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='screen-container'>
+        <div className='forms-container'>
+          //will house the forms
+        </div>
+        <div className='resume-container'>
+          //will house the resume and update with the forms
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
