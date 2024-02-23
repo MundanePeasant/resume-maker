7204671d1b4bfd2110f7e81e65df692a915a20a2
Mundane <mundanepeasant@gmail.com>
2024-02-22 20:53:33 -0600

    Add Experience form bare bones

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import PersonalForm from './components/PersonalForm'
 import EducationForm from './components/EducationForm'
+import ExperienceForm from './components/ExperienceForm'
 import './styles/App.css'
 
 function App() {
@@ -13,6 +14,8 @@ function App() {
           <PersonalForm />
           Education
           <EducationForm />
+          Experience
+          <ExperienceForm />
         </div>
         <div className='resume-container'>
           //will house the resume and update with the forms
---
+++ src/components/ExperienceForm.jsx
@@ -0,0 +1,28 @@
+function ExperienceForm () {
+    return (
+        <div className="experience-form">
+            <form action="">
+                <label htmlFor="company">Company: </label>
+                <input type="text" name="company" id="company" />
+                <br />
+                <label htmlFor="title">Title: </label>
+                <input type="text" name="title" id="title" />
+                <br />
+                <label htmlFor="location">Location: </label>
+                <input type="text" name="location" id="location" />
+                <label htmlFor="start-date">Start Date: </label>
+                <input type="date" name="start-date" id="start-date" />
+                <label htmlFor="end-date">End Date: </label>
+                <input type="date" name="end-date" id="end-date" />
+                <br />
+                <label htmlFor="bullet">Bullets: </label>
+                <textarea name="bullets" id="bullets" cols="30" rows="10"></textarea>
+                <button>Add bullet</button>
+                <br />
+                <button>Add Experience</button>
+            </form>
+        </div>
+    );
+}
+
+export default ExperienceForm;
