c4a3daee75529955f1752f44b86703c773521d92
Mundane <mundanepeasant@gmail.com>
2024-03-01 17:32:53 -0600

    Show experiences on Resume

--- src/components/ExperienceCV.jsx
+++ src/components/ExperienceCV.jsx
@@ -1,9 +1,20 @@
 
 
 function ExperienceCV(props) {
-    console.log(props.experience)
+
+    const experiences = props.experience.map(exp =>
+        <div key={exp.id}>
+            <div>{exp.company}</div>
+            <div>{exp.location}</div>
+            <div>{exp.title}</div>
+            <div>{exp.startDate}</div>
+            <div>{exp.endDate}</div>
+            <div>{exp.bullets.map(bullet => 
+                <div key={bullet}>{bullet}</div>)}</div>
+        </div>)
 
     return (<>
+    {experiences}
     </>)
 }
 
